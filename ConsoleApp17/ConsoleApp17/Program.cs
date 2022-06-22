c446a4167b9cb259e019aaf5db7acd1bec5a2c90
S4kson <108012249+S4kson@users.noreply.github.com>
2022-06-22 22:58:01 +0700

    Add files via upload

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,25 @@
+﻿int Lenght = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int[] Array1 = new int[Lenght];
+Random r = new();
+
+for (int i = 0; i < Lenght; i++)
+{
+    Array1[i] = r.Next(-100, 100);
+    Console.WriteLine(Array1[i]);
+}
+
+Console.WriteLine();
+
+for (int i = Array1.Length - 1; i > 0; i--)
+{
+    int rand = r.Next(i);
+
+    int temp1 = Array1[rand];
+    Array1[rand] = Array1[i];
+    Array1[i] = temp1;
+}
+for (int i = 0; i < Lenght; i++)
+{
+    Console.WriteLine(Array1[i]);
+}
---
+++ УгадайЧисло/УгадайЧисло/Program.cs
@@ -0,0 +1,38 @@
+﻿Random rand = new Random();
+int i = rand.Next(10);
+int count = 1;
+Console.WriteLine("Компьютер загадал число от 0 до 9. Попробуйте отгодать его за три попытки.");
+Console.WriteLine("Введите первое число:");
+int k = Convert.ToInt32(Console.ReadLine());
+while (count <= 3)
+{
+    if (i > k)
+    {
+        Console.WriteLine("Нет, это не число " + k + "! Загаданное число больше. Попытка № " + count + ":");
+
+        k = Convert.ToInt32(Console.ReadLine());
+    }
+    if (i < k)
+    {
+        Console.WriteLine("Нет, это не число " + k + "! Загаданное число меньше. Попытка № " + count + ":");
+
+        k = Convert.ToInt32(Console.ReadLine());
+    }
+    if (i == k)
+    {
+        Console.WriteLine("Да! Компьютер загадал число " + k + "!");
+        break;
+    }
+   
+    else
+    {
+        count++;
+        if (count == 3)
+        {
+            Console.WriteLine("Увы, вы не отгодали загаданное число. Это было число " + i + "!");
+            break;
+        }
+    }
+   
+}
+Console.ReadLine();
